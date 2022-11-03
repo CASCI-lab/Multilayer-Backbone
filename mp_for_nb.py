9cd1b1a8e7a3d77547a33719be31f90ab144a42e
jcrozum <jcrozum@gmail.com>
2022-11-03 14:46:53 -0400

    partial parallelism in DDI example now

--- mp_for_nb.py
+++ mp_for_nb.py
@@ -3,5 +3,10 @@ def appd(arg):
     n = arg[1]
     return n.id, G.pareto_shortest_distances(n,depth_cut=None)
 
+def appd_cut(arg):
+    G = arg[0]
+    n = arg[1]
+    return n.id, G.pareto_shortest_distances(n,depth_cut=5)
+
 def make_arglist(KG,layer):
     return [(KG,KG.nodes[(n_name, layer)]) for n_name in KG.layers[layer].nodes()]
