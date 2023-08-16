bf52d6bfdb088aa3a8775b5a5ce8ee4a0c379d09
jcrozum <jcrozum@gmail.com>
2023-08-16 10:57:43 -0400

    switch computation order in example

--- rust-version/examples/example.py
+++ rust-version/examples/example.py
@@ -43,10 +43,10 @@ graphs = [G_L0, G_L1, G_L2]
 
 ER0 = nx.erdos_renyi_graph(50, 0.05)
 ER1 = nx.erdos_renyi_graph(100, 0.01)
-ER2 = nx.erdos_renyi_graph(45, 0.03)
-ER3 = nx.erdos_renyi_graph(15, 0.1)
+ER2 = nx.erdos_renyi_graph(75, 0.03)
+ER3 = nx.erdos_renyi_graph(25, 0.1)
 ER4 = nx.erdos_renyi_graph(15, 0.1)
-ER5 = nx.erdos_renyi_graph(10, 0.1)
+ER5 = nx.erdos_renyi_graph(20, 0.1)
 graphs = [ER0, ER1, ER2, ER3, ER4, ER5]
 for ER in graphs:
     for u, v in ER.edges():
@@ -104,6 +104,18 @@ for i, G in enumerate(graphs):
 #     for v, weight in sorted(d.items()):
 #         print(index_lookup[u], index_lookup[v], weight)
 print("=" * 20)
+print("BACKBONE (STRUCTURAL)")
+print("- " * 10)
+t0s = time.perf_counter_ns()
+multilayer_backbone_structural = bb.structural_backbone_py(edgelist)
+t1s = time.perf_counter_ns()
+bb_from_structure_edges = set()
+for u, d in sorted(multilayer_backbone_structural.items()):
+    for v, weight in sorted(d):
+        print(index_lookup[u], index_lookup[v], weight)
+        bb_from_structure_edges.add((u, v))
+
+print("=" * 20)
 print("BACKBONE (CLOSURE)")
 print("- " * 10)
 t0b = time.perf_counter_ns()
@@ -115,17 +127,6 @@ for u, d in sorted(multilayer_backbone.items()):
         print(index_lookup[u], index_lookup[v], weight)
         bb_from_closure_edges.add((u, v))
 
-print("=" * 20)
-print("BACKBONE (STRUCTURAL)")
-print("- " * 10)
-t0s = time.perf_counter_ns()
-multilayer_backbone_structural = bb.structural_backbone_py(edgelist)
-t1s = time.perf_counter_ns()
-bb_from_structure_edges = set()
-for u, d in sorted(multilayer_backbone_structural.items()):
-    for v, weight in sorted(d):
-        print(index_lookup[u], index_lookup[v], weight)
-        bb_from_structure_edges.add((u, v))
 
 print("=-" * 20)
 print("-=" * 20)
